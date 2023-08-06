4ca5b8c877ab4fc33179fe01fbfffc744d96d23a
Hemanth <hemantht111@gmail.com>
2023-08-06 18:18:23 -0500

    Commiting code and example

---
+++ List_learn.py
@@ -0,0 +1,20 @@
+"""
+seq=[]
+for i in range(5):
+	ele=input()
+	seq.append(ele)
+print(seq)
+
+
+
+seq=input().split()
+print(seq)
+
+"""
+a=['a','b','c','d','e','f']
+print(a)
+print(a[1:4])
+print(a[1:])
+print(a[:])
+print(a[:6])
+print(a[:9])
